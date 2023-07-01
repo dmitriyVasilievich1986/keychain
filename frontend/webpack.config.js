f5cd8b9eb96597212bb0789eadb97006e7b75f01
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2023-07-01 14:54:33 +0200

    initialize frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = (env, argv) => {
+  return {
+    devtool: argv.mode == "development" ? "source-map" : false,
+
+    output: {
+      path: path.resolve(__dirname, "../static/js"),
+      filename: "main.js",
+    },
+    entry: {
+      main: path.resolve(__dirname, "src/index.js"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+  };
+};
