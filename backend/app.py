2963559b481d9ba26135ed057ba654eebf8aa3cc
dmitriy <dmitriyvasil@gmail.com>
2024-11-09 13:21:13 +0100

    feat: add create_app function Put all initialize code for a Flask application inside create_app function

--- backend/app.py
+++ backend/app.py
@@ -10,17 +10,19 @@ class PasswordApp(Flask):
         self.fernet = Fernet(self.config["SECRET_KEY"])
 
 
-app = PasswordApp(__name__)
+def create_app():
+    app = PasswordApp(__name__)
 
-app.config.from_object("config")
-app.static_url_path = app.config["STATIC_URL_PATH"]
-app.static_folder = app.config["STATIC_FOLDER"]
-app.register_blueprint(base_view)
-app.init_fernet()
+    app.config.from_object("config")
+    app.static_url_path = app.config["STATIC_URL_PATH"]
+    app.static_folder = app.config["STATIC_FOLDER"]
+
+    app.register_blueprint(base_view)
+    app.init_fernet()
+
+    return app
 
 
 if __name__ == "__main__":
-    app.run(
-        host=app.config["APP_HOST"],
-        port=app.config["APP_PORT"],
-    )
+    app = create_app()
+    app.run(host=app.config["APP_HOST"], port=app.config["APP_PORT"])
