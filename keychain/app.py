b9dcc81a8e05a0165698400b5eb906cdd4140d6e
dmitriy <dmitriyvasil@gmail.com>
2024-11-09 18:48:40 +0100

    fix: fix path to `config.py` file

--- keychain/app.py
+++ keychain/app.py
@@ -27,7 +27,7 @@ def create_app() -> PasswordApp:
 
     app = PasswordApp(__name__)
 
-    app.config.from_object("config")
+    app.config.from_object("keychain.config")
     app.static_url_path = app.config["STATIC_URL_PATH"]
     app.static_folder = app.config["STATIC_FOLDER"]
 
