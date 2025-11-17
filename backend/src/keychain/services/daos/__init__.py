76026abc7737c8291dccc2fa134221b31c94b739
dmitriy <dmitriyvasil@gmail.com>
2025-11-17 09:36:03 +0100

    feat: implement FieldDAO for managing Field entities in the database

--- backend/src/keychain/services/daos/__init__.py
+++ backend/src/keychain/services/daos/__init__.py
@@ -1,6 +1,7 @@
 """DAOs for all models."""
 
+from .field import FieldDAO
 from .password import PasswordDAO
 from .user import UserDAO
 
-__all__ = ["PasswordDAO", "UserDAO"]
+__all__ = ["FieldDAO", "PasswordDAO", "UserDAO"]
