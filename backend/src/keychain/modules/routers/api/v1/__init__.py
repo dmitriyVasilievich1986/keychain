30e7bc8c4402f7492db99662759b4293c282cde5
dmitriy <dmitriyvasil@gmail.com>
2025-11-17 09:38:01 +0100

    feat: add field management endpoints to the API for CRUD operations

--- backend/src/keychain/modules/routers/api/v1/__init__.py
+++ backend/src/keychain/modules/routers/api/v1/__init__.py
@@ -4,6 +4,7 @@ __all__ = ["router"]
 
 from fastapi import APIRouter
 
+from .field import router as field_router
 from .password import router as password_router
 from .user import router as user_router
 
@@ -11,3 +12,4 @@ router = APIRouter(prefix="/v1")
 
 router.include_router(user_router)
 router.include_router(password_router)
+router.include_router(field_router)
