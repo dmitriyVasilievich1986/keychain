26a20fce81074f20120fb32bff73d649e171bc32
dmitriy <dmitriyvasil@gmail.com>
2025-11-17 09:42:39 +0100

    feat: add fields attribute to PasswordGetResponseModel for enhanced password detail representation

--- backend/src/keychain/modules/routers/models/response/password/password_get.py
+++ backend/src/keychain/modules/routers/models/response/password/password_get.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from pydantic import Field
 
 from keychain.modules.routers.models.base.response import BaseResponseModel
+from keychain.modules.routers.models.response.field import FieldGetResponseModelSimple
 
 
 class PasswordGetResponseModelSimple(BaseResponseModel):
@@ -24,3 +25,4 @@ class PasswordGetResponseModel(BaseResponseModel):
     created_at: datetime = Field(..., description="The creation date of the password")
     image_url: str | None = Field(None, description="The URL of the image associated with the password")
     user_id: int = Field(..., description="The ID of the user who owns the password")
+    fields: list[FieldGetResponseModelSimple] = Field(..., description="The fields associated with the password")
