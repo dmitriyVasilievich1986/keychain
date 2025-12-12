c9e8403f0bf6539f6a42521e9cbeb5271f98a6aa
dmitriy <dmitriyvasil@gmail.com>
2025-12-12 19:27:36 +0100

    chore: configure TypeScript paths and aliases for frontend modules

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, './src/components'),
+      '@store': resolve(__dirname, './src/store'),
+      '@pages': resolve(__dirname, './src/pages'),
+      '@assets': resolve(__dirname, './src/assets'),
+    },
+  },
+});
