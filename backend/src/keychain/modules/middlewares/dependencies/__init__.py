df81110af2184e4cf845d2135cd780f6d2c11a00
dmitriy <dmitriyvasil@gmail.com>
2025-12-12 10:40:26 +0100

    feat: add user authorization dependency for FastAPI endpoints using JWT tokens

--- backend/src/keychain/modules/middlewares/dependencies/__init__.py
+++ backend/src/keychain/modules/middlewares/dependencies/__init__.py
@@ -1,6 +1,7 @@
 """Dependencies for the application."""
 
+from .authorize_user import authorize_user
 from .get_config import get_config
 from .get_db import get_db
 
-__all__ = ["get_config", "get_db"]
+__all__ = ["authorize_user", "get_config", "get_db"]
