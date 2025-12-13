b98eec047639467631bf664efc2fd860baf6f319
dmitriy <dmitriyvasil@gmail.com>
2025-12-13 07:15:43 +0100

    feat: update password response models to include image URL and use FieldGetResponseModel

--- backend/src/keychain/modules/routers/models/response/password/password_get.py
+++ backend/src/keychain/modules/routers/models/response/password/password_get.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from pydantic import Field
 
 from keychain.modules.routers.models.base.response import BaseResponseModel
-from keychain.modules.routers.models.response.field import FieldGetResponseModelSimple
+from keychain.modules.routers.models.response.field import FieldGetResponseModel
 
 
 class PasswordGetResponseModelSimple(BaseResponseModel):
@@ -15,6 +15,7 @@ class PasswordGetResponseModelSimple(BaseResponseModel):
 
     id: int = Field(..., description="The ID of the password")
     name: str = Field(..., description="The name of the password")
+    image_url: str | None = Field(None, description="The URL of the image associated with the password")
 
 
 class PasswordGetResponseModel(BaseResponseModel):
@@ -25,4 +26,4 @@ class PasswordGetResponseModel(BaseResponseModel):
     created_at: datetime = Field(..., description="The creation date of the password")
     image_url: str | None = Field(None, description="The URL of the image associated with the password")
     user_id: int = Field(..., description="The ID of the user who owns the password")
-    fields: list[FieldGetResponseModelSimple] = Field(..., description="The fields associated with the password")
+    fields: list[FieldGetResponseModel] = Field(..., description="The fields associated with the password")
