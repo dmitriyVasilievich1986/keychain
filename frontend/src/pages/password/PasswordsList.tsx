71bab1f6e1d227e8de8f1de098b0349d9fe18e11
dmitriy <dmitriyvasil@gmail.com>
2025-12-13 11:24:06 +0100

    refactor: simplify PasswordsList page layout and replace Fab with FloatingButton

--- frontend/src/pages/password/PasswordsList.tsx
+++ frontend/src/pages/password/PasswordsList.tsx
@@ -1,11 +1,12 @@
 import AddIcon from '@mui/icons-material/Add';
 import Autocomplete from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
-import Fab from '@mui/material/Fab';
+import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import classnames from 'classnames/bind';
 import { useNavigate, useParams } from 'react-router';
 
+import { FloatingButton } from '@components/floatingButton';
 import { usePasswordsStore, type PasswordSimple } from '@store/passwords';
 import { useUserStore } from '@store/user';
 import { useApiClient } from '@utils/apiClient';
@@ -33,51 +34,46 @@ function PasswordsList() {
 
   return (
     <Box className={cx('passwords-list-container')}>
-      <Box className={cx('passwords-list-autocomplete-container')}>
-        <Box className={cx('passwords-list-autocomplete-box')}>
-          <Autocomplete
-            fullWidth
-            className={cx('passwords-list-autocomplete')}
-            options={passwords}
-            getOptionLabel={(option) => option.name}
-            renderInput={(params) => <TextField {...params} label="Password" />}
-            value={
-              passwordId ? passwords.find((password) => password.id === Number(passwordId)) : null
+      <Stack
+        spacing={2}
+        direction="row"
+        alignItems="center"
+        className={cx('passwords-list-autocomplete-container')}
+      >
+        <Autocomplete
+          fullWidth
+          options={passwords}
+          getOptionLabel={(option) => option.name}
+          renderInput={(params) => <TextField {...params} label="Password" />}
+          value={
+            passwordId ? passwords.find((password) => password.id === Number(passwordId)) : null
+          }
+          onOpen={fetchPasswords}
+          loading={isLoading}
+          onChange={(_, value) => {
+            if (value) {
+              navigate(`/password/${value.id}`);
+            } else {
+              navigate('/password');
             }
-            onOpen={fetchPasswords}
-            loading={isLoading}
-            onChange={(_, value) => {
-              if (value) {
-                navigate(`/password/${value.id}`);
-              } else {
-                navigate('/password');
-              }
-            }}
-            renderOption={(props, option) => (
-              <li {...props} key={option.id}>
-                <img
-                  src={`${import.meta.env.VITE_IMAGES_HOST}/${option.imageUrl}`}
-                  alt={option.name}
-                  style={{ marginRight: '0.5rem' }}
-                  width={25}
-                  height={25}
-                />
-                <span>{option.name}</span>
-              </li>
-            )}
-          />
-          <Box className={cx('passwords-list-fab-container')}>
-            <Fab
-              color="primary"
-              aria-label="add"
-              size="small"
-              onClick={() => navigate('/password/create')}
-            >
-              <AddIcon />
-            </Fab>
-          </Box>
-        </Box>
-      </Box>
+          }}
+          renderOption={(props, option) => (
+            <li {...props} key={option.id}>
+              <img
+                src={`${import.meta.env.VITE_IMAGES_HOST}/${option.imageUrl}`}
+                alt={option.name}
+                style={{ marginRight: '0.5rem' }}
+                width={25}
+                height={25}
+              />
+              <span>{option.name}</span>
+            </li>
+          )}
+        />
+        <FloatingButton onClick={() => navigate('/password/create')} color="primary">
+          <AddIcon />
+        </FloatingButton>
+      </Stack>
     </Box>
   );
 }
