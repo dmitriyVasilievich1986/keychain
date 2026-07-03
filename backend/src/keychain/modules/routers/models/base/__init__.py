c9fdec6afaf35409774afab0ed1078f6ee8927fb
dmitriy <dmitriyvasil@gmail.com>
2026-07-03 17:43:19 +0200

    refactor: Enhance base models and introduce pagination metadata and query structures
    - Updated base models to improve clarity and functionality, including changes to serialization settings.
    - Introduced new pagination metadata and query models to support paginated responses with filters.
    - Added a new metadata schemas module to encapsulate pagination-related models and queries.
    - Enhanced existing models with improved docstrings and type annotations for better documentation and validation.

--- backend/src/keychain/modules/routers/models/base/__init__.py
+++ backend/src/keychain/modules/routers/models/base/__init__.py
@@ -1,7 +1,7 @@
 """Base models for the web API."""
 
+__all__ = ("BaseQueryModel", "BaseRequestModel", "BaseResponseFromModelSchema", "BaseResponseModel")
+
 from .query import BaseQueryModel
 from .request import BaseRequestModel
-from .response import BaseResponseModel
-
-__all__ = ("BaseQueryModel", "BaseRequestModel", "BaseResponseModel")
+from .response import BaseResponseFromModelSchema, BaseResponseModel
