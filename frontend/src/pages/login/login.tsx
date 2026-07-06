f70ed684b5dd23507c150fcdafe213874bf17831
dmitriy <dmitriyvasil@gmail.com>
2026-07-06 17:37:10 +0200

    feat: Refactor login component to use new authentication API
    - Updated the login component to utilize the `useAuthAPIClient` for handling user authentication.
    - Replaced the previous Axios call with a more streamlined login function that sets the access token in cookies.
    - Enhanced error handling to provide clearer feedback on login failures.
    - Renamed the login handler function for clarity and improved code readability.

--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -9,48 +9,43 @@ import InputAdornment from '@mui/material/InputAdornment';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
-import axios from 'axios';
 import classnames from 'classnames/bind';
 import { useState } from 'react';
-import { useNavigate } from 'react-router';
+import { useNavigate, useSearchParams } from 'react-router';
+import Cookies from 'js-cookie';
+import { useAuthAPIClient } from '@utils/apiClient/auth';
 
 import { useUserStore } from '@store/user';
 
 import * as defaultStyle from './style.scss';
-
-import type { LoginResponse } from './types';
 
 const cx = classnames.bind(defaultStyle);
 
 export function Login() {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
 
-  const { setAccessToken } = useUserStore();
+  const { isLoading } = useUserStore();
+
+  const [searchParams] = useSearchParams();
+  const redirectTo = searchParams.get('redirectTo') ?? '/';
+
+  const { login } = useAuthAPIClient();
+
   const navigate = useNavigate();
 
-  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (isLoading) return;
-
-    setIsLoading(true);
-    const data = { username, password };
-    try {
-      const response = await axios.post<LoginResponse>(
-        `${import.meta.env.VITE_API_HOST}/api/v1/user/login`,
-        data
-      );
-      setAccessToken(response.data.accessToken);
-      navigate('/');
-    } catch (error) {
-      console.error(error);
-      setError('Invalid username or password');
-    } finally {
-      setIsLoading(false);
-    }
+    login(username, password)
+      .then((response) => {
+        Cookies.set('accessToken', response.accessToken);
+        navigate(redirectTo);
+      })
+      .catch((error) => {
+        setError(error.response.data.detail || 'An unknown error occurred');
+      });
   };
 
   return (
@@ -71,7 +66,7 @@ export function Login() {
               height={100}
             />
           </Box>
-          <form onSubmit={handleLogin}>
+          <form onSubmit={handleSubmit}>
             <Stack spacing={2} sx={{ marginTop: '1rem' }}>
               <TextField
                 label="Login"
